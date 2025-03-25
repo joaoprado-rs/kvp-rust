72997f5a9913af5e6fe0e239b8b7990e97e4d131
joaoprado-rs <joaoprado275@gmail.com>
2025-03-25 01:25:48 -0300

    Initial implementation of body parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashMap,
+    hash::Hash,
     io::{BufRead, BufReader, Lines, Read, Write},
     net::{TcpListener, TcpStream},
 };
@@ -8,7 +9,7 @@ use std::{
 struct Request {
     path: String,
     method: String,
-    header: HashMap<String, String>,
+    headers: HashMap<String, String>,
     body: Option<String>,
 }
 
@@ -16,13 +17,13 @@ impl Request {
     fn new(
         path: String,
         method: String,
-        header: HashMap<String, String>,
+        headers: HashMap<String, String>,
         body: Option<String>,
     ) -> Self {
         Request {
             path,
             method,
-            header,
+            headers,
             body,
         }
     }
@@ -56,7 +57,7 @@ fn handle_connection(mut stream: TcpStream) {
             request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
             let parsed_request = build_request(&request).or_else(|| {
                 println!("An error has occurred while processing the request...");
-                return;
+                None
             });
         }
         Err(err) => {
@@ -71,9 +72,47 @@ fn handle_connection(mut stream: TcpStream) {
 fn build_request(request_string: &String) -> Option<Request> {
     let mut lines = request_string.lines();
     let first = lines.next()?;
-    let (method, route) = build_method_route(first)?;
+    let (method, path) = build_method_route(first)?;
+    let mut headers: HashMap<String, String> = HashMap::new();
+    for line in lines.by_ref() {
+        if line.is_empty() {
+            break;
+        }
 
-    None
+        let parts = line.split_once(":");
+        if let Some((mut key, mut value)) = parts {
+            key = key.trim();
+            value = value.trim();
+            headers.insert(key.to_string(), value.to_string());
+        } else {
+            println!(
+                "Skiping line because does not match the pattern: '{}'",
+                line.to_string()
+            );
+        }
+    }
+
+    let body = if headers
+        .get("Content-Length")
+        .and_then(|value| value.parse::<u16>().ok())
+        .filter(|&size| size > 0)
+        .is_some()
+        && headers
+            .get("Content-Type")
+            .filter(|content| content.as_str() == "application/json")
+            .is_some()
+    {
+        let body_lines: Vec<&str> = lines.collect();
+        if !body_lines.is_empty() {
+            Some(body_lines.join(""))
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    Some(Request::new(path, method, headers, body))
 }
 
 fn build_method_route(first: &str) -> Option<(String, String)> {
@@ -82,5 +121,3 @@ fn build_method_route(first: &str) -> Option<(String, String)> {
     let route = vec_line.get(1).map(|s| s.to_string())?;
     Some((method, route))
 }
-
-fn build_headers()
